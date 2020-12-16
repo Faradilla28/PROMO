e7711a0eb5cb91de40f166a5b8f5d86fcb1335ef
Faradilla28 <faradilla.2838@students.amikom.ac.id>
2020-12-16 20:16:22 +0700

    Add project files.

---
+++ PROMO/Controller/MainWindowController.cs
@@ -0,0 +1,31 @@
+﻿using PROMO.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PROMO.Controller
+{
+    class MainWindowController
+    {
+        KeranjangBelanja keranjangBelanja;
+
+        public MainWindowController(KeranjangBelanja keranjangBelanja)
+        {
+            this.keranjangBelanja = keranjangBelanja;
+        }
+
+        public void addItem(Item item)
+        {
+            this.keranjangBelanja.addItem(item);
+        }
+        public List<Item> getSelectedItems()
+        {
+            return this.keranjangBelanja.getItems();
+        }
+
+        public void deleteSelectedItem(Item item)
+        {
+            this.keranjangBelanja.removeItem(item);
+        }
+    }
+}
---
+++ PROMO/Controller/PenawaranController.cs
@@ -0,0 +1,32 @@
+﻿using PROMO.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PROMO.Controller
+{
+    class PenawaranController
+    {
+        private List<Item> items;
+
+        public PenawaranController()
+        {
+            items = new List<Item>();
+        }
+
+        public void AddItem(Item item)
+        {
+            this.items.Add(item);
+        }
+
+        public List<Item> getItems()
+        {
+            return this.items;
+        }
+
+        internal void addItem(Item drink1)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
